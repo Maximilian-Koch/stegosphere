37528b8d363b29b1c22918174b9d5a708b4e2ea4
Maximilian-Koch <164555674+Maximilian-Koch@users.noreply.github.com>
2024-12-11 23:17:16 +0100

    initial commit

---
+++ stegosphere/__init__.py
@@ -0,0 +1,9 @@
+from spatial import *
+from utils import *
+
+__all__ = ['BaseCoder', 'BaseLSB', 'BaseVD', 'CrossVD',
+           'binary_to_data', 'binary_to_file', 'data_to_binary', 'file_to_binary']
+
+__author__ = 'Maximilian J. W. Koch'
+
+__version__ = '1.0'
