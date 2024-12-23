efdb6584dee85ec7ad37954fe78ff983c652c82d
Maximilian Koch <164555674+Maximilian-Koch@users.noreply.github.com>
2024-12-23 17:28:19 +0100

    Update image.py

--- stegosphere/image.py
+++ stegosphere/image.py
@@ -6,12 +6,15 @@ except:
                   UserWarning)
 import numpy as np
 
+import core
 from utils import *
+import utils
 from spatial import BaseLSB, BaseVD
+from transform import BaseIWT
 
-__all__ = ['LSB', 'PVD', 'METADATA_LENGTH_IMAGE']
+__all__ = ['IWT', 'LSB', 'PVD', 'METADATA_LENGTH_IMAGE']
 
-class ImageHandler(File):
+class ImageHandler(core.Container):
     """
     A base class for handling image data.
 
@@ -21,16 +24,16 @@ class ImageHandler(File):
     def __init__(self, image):
         super().__init__(image)
     def _read_file(self, path):
-        self.image = Image.open(path)
-        return np.array(self.image)
+        self.obj = Image.open(path)
+        return np.array(self.obj)
     def _flush_file(self):
         if len(self._shape) <= 2: #grayscale
             data = self.data.flatten()
         else: #multiple channels
             data = [tuple(pixel) for row in self.data for pixel in row]
-        self.image.putdata(data)
+        self.obj.putdata(data)
     def _save_file(self, path):
-        self.image.save(path)
+        self.obj.save(path)
 
 class LSB(ImageHandler, BaseLSB):
     """
@@ -39,9 +42,9 @@ class LSB(ImageHandler, BaseLSB):
     :param image: The image data, either a NumPy array, file path, or list/tuple.
     :type image: numpy.ndarray, str, list, or tuple
     """
-    def __init__(self, image):
+    def __init__(self, image, analysis=utils.ANALYSIS):
         ImageHandler.__init__(self, image)
-        BaseLSB.__init__(self, self.data)
+        BaseLSB.__init__(self, self.data, analysis)
 
 class PVD(ImageHandler, BaseVD):
     """
@@ -50,7 +53,7 @@ class PVD(ImageHandler, BaseVD):
     :param image: The image data, either a NumPy array, file path, or list/tuple.
     :type image: numpy.ndarray, str, list, or tuple
     """
-    def __init__(self, image):
+    def __init__(self, image, analysis=utils.ANALYSIS):
         ImageHandler.__init__(self, image)
         if len(self._shape) == 3:
             pos_dim = 2
@@ -61,5 +64,10 @@ class PVD(ImageHandler, BaseVD):
         else:
             #this library does not support voxels (yet)
             raise Exception('unsupported image shape')
-        BaseVD.__init__(self, self.data, pos_dim, depth)
+        BaseVD.__init__(self, self.data, pos_dim, depth, analysis)
 
+
+class IWT(ImageHandler, BaseIWT):
+    def __init__(self, image):
+        ImageHandler.__init__(self, image)
+        BaseIWT.__init__(self, self.data)
